c4f63e2009173e2d7bd0769ceb844b13b7a1b524
Michael MiDri <riddlemd@gmail.com>
2023-07-04 18:56:31 -0500

    Added Jynx.Cli

---
+++ Jynx.Cli/Commands/ApiAppCommands.cs
@@ -0,0 +1,27 @@
+﻿using Jynx.Common.Abstractions.Services;
+using Jynx.Common.Entities;
+
+namespace Jynx.Cli.Commands
+{
+    [Command("api-app")]
+    internal class ApiAppCommands : ConsoleAppBase
+    {
+        private readonly IApiAppService _apiAppService;
+
+        public ApiAppCommands(IApiAppService apiAppService)
+        {
+            _apiAppService = apiAppService;
+        }
+
+        public async Task Create(string name, string ownerId)
+        {
+            var entity = new ApiApp
+            {
+                Name = name,
+                UserId = ownerId
+            };
+
+            await _apiAppService.CreateAsync(entity);
+        }
+    }
+}
---
+++ Jynx.Cli/Program.cs
@@ -0,0 +1,48 @@
+﻿using Jynx.Cli.Commands;
+using Jynx.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Jynx.Cli
+{
+    internal class Program
+    {
+        private const string _defaultCommand = "api-app create --name Test --owner-id Test";
+
+        static async Task Main(string[] args)
+        {
+#if DEBUG
+            args = _defaultCommand.Split(' ');
+#endif
+
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+
+            Console.WriteLine($"Jynx.Cli {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}");
+
+            var builder = ConsoleApp.CreateBuilder(args)
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddUserSecrets<Program>();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services
+                        .AddCommon(context.Configuration)
+                        .AddSingleton<ISystemClock, SystemClock>();
+                });
+            
+            var app = builder.Build();
+
+            app.AddSubCommands<ApiAppCommands>();
+
+            await app.RunAsync();
+        }
+    }
+}
