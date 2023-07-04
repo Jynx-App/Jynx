77b4c34bf937a09f46480cf0867dea72211b5e04
Michael MiDri <riddlemd@gmail.com>
2023-07-04 17:19:02 -0500

    Added ObjectExtensions

---
+++ Jynx.Common/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+
+namespace Jynx.Common
+{
+    public static class ObjectExtensions
+    {
+        public static dynamic ToDynamic(this object obj)
+        {
+            IDictionary<string, object?> eObj = new ExpandoObject()!;
+
+            var objProps = obj.GetType().GetProperties();
+
+            foreach (var prop in objProps)
+            {
+                eObj[prop.Name] = prop.GetValue(obj);
+            }
+
+            return eObj;
+        }
+    }
+}
