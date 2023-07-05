90117a0697f3a2a42bffcaafb5654540ac208a9c
Michael MiDri <riddlemd@gmail.com>
2023-07-05 12:15:23 -0500

    Fixed model

--- Jynx/Models/District.cs
+++ Jynx/Models/District.cs
@@ -4,8 +4,8 @@
     {
         public string? Id { get; set; }
 
-        public string Name { get; set; } = default;
+        public string Name { get; set; } = "";
 
-        public string Description { get; set; } = default;
+        public string Description { get; set; } = "";
     }
 }
